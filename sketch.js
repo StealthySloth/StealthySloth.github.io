fb3259be777ebe432f5f0787d988422f67d22679
StealthySloth <leighconnor@rocketmail.com>
2017-12-03 17:24:03 +0000

    Styling webpage more

--- sketch.js
+++ sketch.js
@@ -1,38 +1,14 @@
 //DAT405 / GAD405
-//09_Exercise 4 - Color Coordinates
 
 function setup() {
-  var text = createDiv('GAD405 - Colour Coordinates')
-  text.position(50, 50);
-
   var canvas = createCanvas(594, 841);
   stroke(0);
   canvas.parent("myContainer");
   //canvas.position(300, 50);
   canvas.class("myCanvas");
+
 }
 
 function draw() {
-  background(0); //Set your background color to black (0)
-
-//Top-left square
-  fill(mouseX, mouseY, 255, 155, 100)
-  rect(0, 0, 400, 400)
-  noFill()
-
-//Bottom-Left square
-  fill(mouseX, mouseY, 175, 100, 55)
-  rect(0, 200, 400, 400)
-  noFill()
-
-//Top-Right square
-  fill(mouseX, mouseY, 125, 155, 175)
-  rect(200, 0, 400, 400)
-  noFill()
-
-//Bottom-Right square
-  fill(mouseX, mouseY, 50, 85, 125)
-  rect(200, 200, 400, 400)
-  noFill()
 
 }
